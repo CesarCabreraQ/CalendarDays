2dc0ea422c9905025c56d9df6ccb556ded068d17
César Cabrera <cesarcabreraq@gmail.com>
2017-10-02 18:09:17 -0500

    Show Min and Max date on Calendar
    Show Min and Max date on Calendar considering the number of days.

--- myCalendarDays.js
+++ myCalendarDays.js
@@ -11,9 +11,27 @@ $(document).ready(function () {
 
 
      let showCalendar  = function(){
-         let nroDays = 0;
-         nroDays = $("#numOfDays").val();
+         
+        let nroDays = 0;
+        if($("#numOfDays").val().toString().trim() == ""){
+            nroDays = 0;
+        }else{
+            nroDays = parseInt($("#numOfDays").val());
+        }
+
+         
       
+         let  date1 = $("#datepicker").val();
+         let date2 = $("#datepicker").datepicker('getDate');
+         date2.setDate(date2.getDate() + nroDays - 1);
+         console.log(date2);
+         date2 = (date2.getMonth() + 1) + '/' + date2.getDate() + '/' + date2.getFullYear();
+
+         $("#datepicker1").datepicker("destroy");
+         $("#datepicker1").datepicker({});
+         $("#datepicker1").datepicker("option", "minDate", date1);
+         $("#datepicker1 ").datepicker("option","maxDate",date2);
+
      }
 
 
